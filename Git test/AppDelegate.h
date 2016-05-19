d7e7583d42288bffbe2d7677bc2baae07b549bea
Martin Denci <martindenci@martins-macbook-air.local>
2016-05-19 23:07:02 +0200

    Initial Commit

---
+++ Git test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git test
+//
+//  Created by Martin Denci on 19/05/16.
+//  Copyright © 2016 Martin Denci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
