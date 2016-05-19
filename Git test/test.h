c1f41dee8b56b4f1553c76b9a48a12c7b55cbaf0
Martin Denci <martindenci@Martins-MacBook-Air.local>
2016-05-19 23:35:01 +0200

    add Class Test

---
+++ Git test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Git test
+//
+//  Created by Martin Denci on 19/05/16.
+//  Copyright © 2016 Martin Denci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ Git test/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Git test
+//
+//  Created by Martin Denci on 19/05/16.
+//  Copyright © 2016 Martin Denci. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
